9718aeb587d17164ca75d1f64e12914e00c4d71e
sistemas-gobvalle <amartincas75@gmail.com>
2026-07-30 15:18:50 -0500

    User Panel III

--- app/Filament/Resources/Leads/Pages/EditLead.php
+++ app/Filament/Resources/Leads/Pages/EditLead.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Leads\Pages;
 use App\Filament\Resources\Leads\LeadResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Auth;
 
 class EditLead extends EditRecord
 {
@@ -13,7 +14,8 @@ class EditLead extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            DeleteAction::make(),
+            DeleteAction::make()
+                ->visible(fn () => Auth::user()?->is_super_admin ?? false),
         ];
     }
 }
